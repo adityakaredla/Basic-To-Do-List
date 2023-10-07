4db69a18a17eb20f43ab8787605c842c650c3571
adityakaredla <aditya.karedla@gmail.com>
2023-10-07 11:37:26 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Add a task to Continue!!");
+    }
+
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = "<p>"+inputBox.value+"</p>";
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+
+    }
+
+    inputBox.value = '';
+    saveData();
+}
+
+listContainer.addEventListener('click',function(e){
+    if(e.target.tagName === 'P'){
+        e.target.classList.toggle('checked');
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+    }
+    saveData();
+},false);
+
+function saveData(){
+    localStorage.setItem('tasks',listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem('tasks');
+}
+
+showTask();
